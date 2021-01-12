c2d98216a7a0c790305f68808fb14605b578ad6b
Mako3 <big.mako@gmail.com>
2021-01-11 18:59:04 -0800

    updated auth to passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -46,13 +48,15 @@ app.use(session({
   store: new FileStore(),
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 auth = (req, res, next) => {
-  console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
       let err = new Error("You are not authenticated!");
 
       res.setHeader("WWW-Authenticate", "Basic");
@@ -60,14 +64,7 @@ auth = (req, res, next) => {
       return next(err);
 
   } else {
-    if (req.session.user === "authenticated") {
       next();
-    } else {
-      let err = new Error("You are not authenticated!");
-
-      err.status = 403;
-      return next(err);
-    }
   }
 };
 
